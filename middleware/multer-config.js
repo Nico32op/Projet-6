2954fc84f348127ad90fd59e363eba0b62fee3eb
Nico32op <leonardi.nicolas@live.fr>
2021-04-20 11:42:26 +0200

    maj 20-04-21 11h42

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -16,8 +16,9 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     //filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier. Elle utilise ensuite la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée
     const name = file.originalname.split(" ").join("_");
+    const finalName = name.split(".")[0];
     const extension = MIME_TYPES[file.mimetype]; //notre liste d'exentions correspond aux liste reçu par le frontend
-    callback(null, name + Date.now() + "." + extension); //Date.now() est un timestamp permettant d'afficher les millisecondes écoulé depuis le 1er janvier 1970
+    callback(null, finalName + Date.now() + "." + extension); //Date.now() est un timestamp permettant d'afficher les millisecondes écoulé depuis le 1er janvier 1970
   },
 });
 
